786a09f1876919a339832d066d78501adf0bd3ab
jsmld <samvanerpersroijaards@gmail.com>
2023-12-02 12:51:49 -0300

    Part 1 of Day 1 completed

--- 1/index.js
+++ 1/index.js
@@ -1,5 +1,21 @@
-import fs, { readFileSync } from 'fs'
+import { readFileSync } from 'fs'
 
-const calVals = readFileSync('./calibration.txt', 'utf-8').split('\n')
+const calibrationValues = readFileSync('./calibration.txt', 'utf-8').split('\n')
 
-console.log(calVals)
+function numbersOnly (str) {
+  return str.split('').filter(c => !isNaN(c)).join('')
+}
+
+function firstAndLast (str) {
+  return str[0] + str[str.length - 1]
+}
+
+function sum (acc, cur) {
+  return parseInt(acc) + Number(cur)
+}
+
+function calibrationSum (inputArr) {
+  return inputArr.map(numbersOnly).map(firstAndLast).reduce(sum)
+}
+
+console.log({'Should be the answer: ': calibrationSum(calibrationValues)})
